e76fd5086c5bf4cce9ab814ec375a01dd6cb0c17
Zain-MKhan <zmkhan025@gmail.com>
2021-02-25 19:54:38 -0500

    #95-Fixed-Login-Component

--- front-end/src/components/Login/Login.jsx
+++ front-end/src/components/Login/Login.jsx
@@ -3,33 +3,44 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import './Login.css';
 import {Logo} from '../Icons/Icons';
+import Modal from 'react-bootstrap/Modal';
 
-const Login = () => {
+const Login = (props) => {
+
+    
+    
     return (
-        <div id="form">
-            <Form>
-                <div id="logo"><Logo /></div>
-                
-                <div id="inputs">
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Username" />
-                    </Form.Group>
-                
-                    <Form.Group controlId="formBasicPassword" >
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                
-                    <div id="loginbutton">
-                        <Button variant="primary" type="submit" id="loginbutton">
-                            Log In
-                        </Button>
+        <Modal.Dialog {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" backdrop="static" keyboard={false} centered>
+        <Modal.Body id="post-container">
+            {/* <div id="form"> */}
+                <Form>
+                    <div id="logo"><Logo /></div>
+                    
+                    <div id="inputs">
+                        <Form.Group controlId="formBasicEmail">
+                            <Form.Control type="text" placeholder="Username" />
+                        </Form.Group>
+                    
+                        <Form.Group controlId="formBasicPassword" >
+                            <Form.Control type="password" placeholder="Password" />
+                        </Form.Group>
+                    
+                        <div id="loginbutton">
+                            <Button variant="primary" type="submit" id="loginbutton">
+                                Log In
+                            </Button>
+                        </div>
                     </div>
-                </div>
-                <hr/>
 
-                <p id="signup">Don't have an account? &nbsp;<a href="#Registration"> Sign up</a></p>            
-            </Form>
-      </div>
+                    <hr/>
+
+                    <p id="signup">Don't have an account? &nbsp;<a href="#Registration"> Sign up</a></p>            
+                </Form>
+            {/* </div> */}
+        </Modal.Body>
+      </Modal.Dialog>
+        
+       
     )
 }
 
