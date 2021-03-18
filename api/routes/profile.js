4d193b9953308a8d77d054f598610dc4cdafef02
kknicekewl <justin.latella0109@gmail.com>
2021-03-17 21:35:16 -0400

    fixed frontend and backend session communication

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -5,15 +5,21 @@ var session = require('express-session');
 
 let User = require('../data_access_layer/usersDAO');
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-    if (!req.session.user){
-        return res.status(403).send("You are not logged in");
-    }
-    res.render('profile', { title: 'Express', thing: req.session.user.username });
-    return res.status(200).send(req.session.user.username);
+  console.log(req.session.user);
+    const useernamereq = req.session.user;
+    res.status(200).send(useernamereq);
   });
 
-  router.post('/', function(req, res, next){
-     
-  })
+router.post('/', function(req, res, next){
+    if (!req.session.user){
+      console.log("**NO SESSION POST**");
+      res.status(403).send("You are not logged in");
+      return;
+  }
+  console.log("**IN SESSION POST**");
+  res.status(200).send(req.session.user);
+  return;
+  });
 module.exports = router;
